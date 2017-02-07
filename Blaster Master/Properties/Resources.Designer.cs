28435d70b6a83f4bedafdadd33801b03fa3471fc
ItsJeffrey <jeffreythwaitez@gmail.com>
2017-02-07 11:04:49 -0700

    Properties?

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -343,6 +343,16 @@ namespace BlasterMaster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap pickupJeff {
+            get {
+                object obj = ResourceManager.GetObject("pickupJeff", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playerA {
             get {
                 object obj = ResourceManager.GetObject("playerA", resourceCulture);
